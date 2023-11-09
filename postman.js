4fc3d22b68894a88d38f9195eb99a0958b83fc96
Yasu info <39099655+Yasuinfo@users.noreply.github.com>
2023-11-09 08:47:44 +0300

    Update postman.js

--- postman.js
+++ postman.js
@@ -1,10 +1,21 @@
 document.getElementById('sendRequest').addEventListener('click', function() {
-  var url = document.getElementById('url').value;
+  var baseUrl = document.getElementById('url').value;
   var method = document.getElementById('method').value;
   var headers = JSON.parse(document.getElementById('headers').value);
   var auth = document.getElementById('auth').value;
   var payload = document.getElementById('payload').value;
+  var queryParams = document.getElementById('queryParams').value; // New input field for query params
   var responseDiv = document.getElementById('response');
+
+  // Create the URL with query parameters
+  var url = new URL(baseUrl);
+
+  if (queryParams) {
+    queryParams = new URLSearchParams(queryParams);
+    queryParams.forEach((value, key) => {
+      url.searchParams.append(key, value);
+    });
+  }
 
   var options = {
     method: method,
@@ -19,7 +30,7 @@ document.getElementById('sendRequest').addEventListener('click', function() {
     options.body = payload;
   }
 
-  fetch(url, options)
+  fetch(url.toString(), options)
     .then(response => response.text())
     .then(data => responseDiv.innerHTML = data)
     .catch(error => responseDiv.innerHTML = 'Error: ' + error.message);
